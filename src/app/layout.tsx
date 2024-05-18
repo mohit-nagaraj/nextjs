b81923cf4756019024bf7ef3370f8a2a71016724
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-05-18 15:36:34 +0530

    landing page

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/util";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "QuillBot",
+  description: "QuillBot is a state-of-the-art paraphrasing tool. It is the best article rewriter available, and can completely paraphrase an entire article for free. Simply input a sentence, and hit the 'Quill It' button. QuillBot will then rephrase the content while maintaining the original meaning.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="light">
+      <body
+        // merge the classes using the cn function with the inter font
+        className={cn(
+          "min-h-screen font-sans antialiased grainy",
+          inter.className
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
