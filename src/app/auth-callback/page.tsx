b596285d6a7b6c13186abed434210943f069dc23
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-05-18 19:37:17 +0530

    Update page.tsx

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,10 +1,11 @@
 "use client"
 
+import { Suspense } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
 import { trpc } from '../_trpc/client'
 import { Loader2 } from 'lucide-react'
 
-const Page = () => {
+const AuthCallbackPage = () => {
   const router = useRouter()
   const searchParams = useSearchParams()
   const origin = searchParams.get('origin')
@@ -35,5 +36,22 @@ const Page = () => {
     </div>
   )
 }
+
+const Page = () => {
+  return (
+    <Suspense fallback={<Loader />}>
+      <AuthCallbackPage />
+    </Suspense>
+  )
+}
+
+const Loader = () => (
+  <div className='w-full mt-24 flex justify-center'>
+    <div className='flex flex-col items-center gap-2'>
+      <Loader2 className='h-8 w-8 animate-spin text-zinc-800' />
+      <h3 className='font-semibold text-xl'>Loading...</h3>
+    </div>
+  </div>
+)
 
 export default Page
