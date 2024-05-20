7191408d085f74093fb7ffa6af8345daab75f9cd
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-05-20 07:29:30 +0530

    fix type error from build

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,10 +1,12 @@
 import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server'
-import { NextRequest } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(
   request: NextRequest,
   { params }: any
 ) {
   const endpoint = params.kindeAuth
-  return handleAuth(request, endpoint)
+  const handler = await handleAuth(request, endpoint)
+  const response = await handler(request, {} as any) // Assuming the second argument is an empty object
+  return NextResponse.json(response)
 }
